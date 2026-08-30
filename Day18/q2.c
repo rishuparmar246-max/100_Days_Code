852a37a01ced993fbd7e940e5226794e5679eb67
rishuparmar246-max <rishuparmar246@gmail.com>
2026-08-31 00:54:35 +0530

    Day18

---
+++ Day18/q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+
+    printf("%d", a);
+    return 0;
+}
